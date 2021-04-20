fb07cc062c6e6b08dce0ce97af9b94aae314cb5e
jo030225 <whwngur123@naver.com>
2021-04-20 20:21:08 +0900

    [ADD] NextButton
    Home으로 넘어가는 NextButton 기능 구현

--- GithubTest/GithubTest/LoginViewController.swift
+++ GithubTest/GithubTest/LoginViewController.swift
@@ -12,18 +12,14 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButton(_ sender: Any) {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "a")
+        controller?.modalPresentationStyle = .fullScreen
+        present(controller!, animated: true, completion: nil)
     }
-    */
-
+    
+    
 }
